672f93dd2c8b009af4e282df48135ebe0ff81ab5
Giuliano Sampaio <giuguitar@gmail.com>
2021-05-01 14:24:13 -0300

    rollback command test

---
+++ tests/Feature/MigrateRollbackCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Maestriam\Maestro\Tests\Feature;
+
+use Maestriam\Maestro\Tests\TestCase;
+
+class MigrateRollbackCommandTest extends TestCase
+{
+    public function testMigrateCmd()
+    {
+        $module = 'Canis';
+
+        $message = 'Module rollback.';
+
+        $execute = ['module' => $module];
+
+        $migrate = ['module' => $module, 'name' => 'AddMajorSirius'];
+
+        $this->artisan('maestro:migration', $migrate);
+
+        $this->artisan('maestro:migrate', $execute);
+
+        $this->artisan('maestro:rollback', $execute)->expectsOutput($message);
+    }
+}
