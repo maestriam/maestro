d72feff3e4d0ddfb64da13811aead16ca78b8cba
Giuliano Sampaio <giuguitar@gmail.com>
2023-02-04 20:36:06 -0300

    bugfix de file register

--- src/Foundation/Registers/FileRegister.php
+++ src/Foundation/Registers/FileRegister.php
@@ -2,19 +2,21 @@
 
 namespace Maestriam\Maestro\Foundation\Registers;
 
+use Maestriam\FileSystem\Support\FileSystem;
 use Maestriam\Maestro\Exceptions\InvalidPathToRegisterFileException;
 
 abstract class FileRegister
 {
     static public function from(string $path)
-    {        
+    {     
+        $path = FileSystem::folder($path)->sanitize();
+        
         if (! is_dir($path)) {
             throw new InvalidPathToRegisterFileException($path);
         }
         
         foreach (glob($path ."*.php") as $filename)
         { 
-            dump($filename);
             include_once($filename);                
         }
     }
